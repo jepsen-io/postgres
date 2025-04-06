5461a4bedd520faa9eaa701633ebf6202cda6125
Kyle Kingsbury <aphyr@aphyr.com>
2025-04-06 14:52:41 -0500

    RDS: dependency upgrades

--- rds/project.clj
+++ rds/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jepsen-io/postgres"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [io.jepsen/postgres "0.1.1-SNAPSHOT"
                   :exclusions [; Fights with aws-api
                                org.eclipse.jetty/jetty-http
