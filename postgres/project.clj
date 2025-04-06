a29e5fc085c83ffcc030a88958b46ccb8160160b
Kyle Kingsbury <aphyr@aphyr.com>
2025-04-06 14:52:10 -0500

    Dependency bumps

--- postgres/project.clj
+++ postgres/project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/jepsen-io/postgres"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.6-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [jepsen "0.3.8"]
                  [seancorfield/next.jdbc "1.2.659"]
-                 [org.postgresql/postgresql "42.7.2"]
-                 [cheshire "5.12.0"]
+                 [org.postgresql/postgresql "42.7.5"]
+                 [cheshire "5.13.0"]
                  [clj-wallhack "1.0.1"]]
   :main jepsen.postgres.cli
   :jvm-opts ["-Djava.awt.headless=true"
