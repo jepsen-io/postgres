7f05408eda6d4d7db11f81798be88504842e1a02
Kyle Kingsbury <aphyr@aphyr.com>
2026-05-18 11:11:37 -0500

    Dep bumps
    Clojure 1.12.5
    Postgresql 42.7.11
    Cheshire 6.2.0

--- postgres/project.clj
+++ postgres/project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/jepsen-io/postgres"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.4"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  [jepsen "0.3.12-SNAPSHOT"]
                  [seancorfield/next.jdbc "1.2.659"]
-                 [org.postgresql/postgresql "42.7.5"]
-                 [cheshire "5.13.0"]
+                 [org.postgresql/postgresql "42.7.11"]
+                 [cheshire "6.2.0"]
                  [clj-wallhack "1.0.1"]]
   :main jepsen.postgres.cli
   :aot [jepsen.postgres.cli]
